6772055372392842054dd98eb029af44925b9b59
HsiaoHungWang <wang@ispan.com.tw>
2024-08-10 14:35:08 +0800

    JSON 練習

--- home/views.py
+++ home/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html',{'title':'async await'})
+def json_demo(request):
+    return render(request, 'home/json.html',{'title':'JSON 練習'})
