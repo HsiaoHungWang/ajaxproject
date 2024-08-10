643f6d4d151840fa0c5c9955653ba678ecd473d4
HsiaoHungWang <wang@ispan.com.tw>
2024-08-10 15:24:40 +0800

    setting.py 的資料庫設定

--- ajaxproject/settings.py
+++ ajaxproject/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'ajaxproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'P@ssw0rd',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
