8371297169af9bbc5024ddb57fe75b84d521c0f4
HsiaoHungWang <wang@ispan.com.tw>
2024-08-13 21:14:47 +0800

    顯示隱藏執行中圖示

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+import time
 from .models import Address
 # Create your views here.
 def index(reqeust):
+    time.sleep(5)
     content='Hello Fetch!!'
     # response = HttpResponse(content, 'text/plain')
     # 回傳文字內容
